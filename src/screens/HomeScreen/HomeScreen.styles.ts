cf08eb1930557107d5c247efaa0d00045ee29369
Adil Allahverdiyev <adil@Adils-MacBook-Air.local>
2022-08-31 13:52:10 +0100

    navigation package added

---
+++ src/screens/HomeScreen/HomeScreen.styles.ts
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ViewStyle } from 'react-native'
+
+const backgroundContainer: ViewStyle = {
+    backgroundColor:'green',
+    flex:1
+}
+
+export const styles = {
+    backgroundContainer
+}
---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+    View
+} from 'react-native'
+import { styles } from './HomeScreen.styles'
+
+const HomeScreen = () => {
+    return(
+        <View style={styles.backgroundContainer}/>
+    )
+}
+
+export default HomeScreen
