9ce71a5c0e4170466a74e9e8f12051b12ea45619
Adil Allahverdiyev <Adil.Allahverdiyev@axapppuk.onmicrosoft.com>
2022-08-31 17:27:29 +0100

    navigation started

---
+++ src/navigation/AppDrawer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen/HomeScreen';
+
+export type StackParams = {
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<StackParams>();
+
+const AppDrawer = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppDrawer;
