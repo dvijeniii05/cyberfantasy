32c76b1d9d8606a9866709f4f858fd2d8d788217
Adil Allahverdiyev <Adil.Allahverdiyev@axapppuk.onmicrosoft.com>
2022-09-10 16:38:16 +0100

    🎨 products flatlsit

--- src/components/Products/Products.style.ts
+++ src/components/Products/Products.style.ts
@@ -1,12 +1,20 @@
 import {ViewStyle} from 'react-native';
 
 const parentContainer: ViewStyle = {
-  backgroundColor: 'red',
-  height: 100,
+  backgroundColor: 'green',
+  height: 150,
   marginTop: 20,
   marginHorizontal: 25,
 };
 
+const productContainer: ViewStyle = {
+  backgroundColor: 'red',
+  width: 100,
+  marginHorizontal: 5,
+  justifyContent: 'space-between',
+};
+
 export const styles = {
   parentContainer,
+  productContainer,
 };
--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import {FlatList, View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
+import DUMMY_POSTS from '../../constants/homeScreenData';
 import {styles} from './Products.style';
 
+interface ItemProps {
+  desc: string;
+  image: string;
+}
+
 const Products = () => {
+  const ProductCard = (props: ItemProps) => {
+    return (
+      <View style={styles.productContainer}>
+        <Text>{props.image}</Text>
+        <Text>{props.desc}</Text>
+      </View>
+    );
+  };
   return (
     <View style={styles.parentContainer}>
-      <FlatList />
+      <FlatList
+        data={DUMMY_POSTS}
+        renderItem={({item}) => ProductCard(item)}
+        horizontal
+      />
     </View>
   );
 };
