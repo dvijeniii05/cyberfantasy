57e2d271acc2d6dd3a7d87d32247f67991a4ca71
Adil <adikbsw@yahoo.com>
2022-12-26 15:22:32 +0000

    issue fixed with usetranslation hook

--- src/constants/KnownIssuesData.ts
+++ src/constants/KnownIssuesData.ts
@@ -1,32 +1,29 @@
-import {useTranslation} from 'react-i18next';
-
-// eslint-disable-next-line react-hooks/rules-of-hooks
-const {t} = useTranslation();
+import i18next from "i18next";
 
 export const KnownIssuesData = [
   {
     key: 0,
-    text: t('knownType_DrySkin_text'),
+    text: i18next.t('knownType_DrySkin_text') ,
     value: 'dry%20skin'
   },
   {
     key: 1,
-    text: t('knownType_OilySkin_text'),
+    text:"Oily skin - your face can get shiny as the day progresses. Pores may be more visible and are more likely to suffer acne breakouts.",
     value: 'oily%20skin'
   },
   {
     key: 2,
-    text: t('knownType_CombinedSkin_text'),
+    text: "Combined skin - can be dry or normal in some areas, but oily in others, like the T-zone.",
     value: 'combination%20skin'
   },
   {
     key: 3,
-    text: t('knownType_NormalSkin_text'),
+    text: "Normal skin - regular texture, no or few imperfections and a soft appearance. Well-balanced.",
     value: 'normal%20skin'
   },
   {
     key: 4,
-    text: t('knownType_AgingSkin_text'),
+    text: "Aging skin - looks thinner, paler and clear. Pigmented spots may appear in sun-exposed areas.",
     value: 'anti%20aging'
   },
 ];
