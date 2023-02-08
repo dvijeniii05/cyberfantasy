1505a2c0027a9194496c2369852350dc61869e0c
Adil Allahverdiyev <Adil.Allahverdiyev@axapppuk.onmicrosoft.com>
2023-02-08 18:14:44 +0000

    🚧 carousel dummies added

--- src/screens/MultipleChoiceTestScreen/MultipleChoiceTestScreen.tsx
+++ src/screens/MultipleChoiceTestScreen/MultipleChoiceTestScreen.tsx
@@ -11,6 +11,8 @@ import {COLORS} from '../../../COLORS';
 import {WIDTH} from '../../constants/dimension';
 import {addAnswer} from '../../redux/slices/testsSlice';
 import {AppDispatch, RootState} from '../../redux/stores/mainStore';
+// import CustomCarousel from 'carousel-with-pagination-rn/CustomCarousel';
+// import SlideItem from './SlideItem';
 
 interface AnswerType {
   id: number;
@@ -59,6 +61,30 @@ const dummyQuestions: Array<QuestionType> = [
     ],
   },
 ];
+
+// const dummyCarousel = [
+//   {
+//     id: 1,
+//     img: require('../../assets/carouselStuff/image1.jpeg'),
+//     title: 'Apple Watch Series 7',
+//     description: 'The future of health is on your wrist',
+//     price: '$399',
+//   },
+//   {
+//     id: 2,
+//     img: require('../../assets/carouselStuff/image2.png'),
+//     title: 'AirPods Pro',
+//     description: 'Active noise cancellation for immersive sound',
+//     price: '$249',
+//   },
+//   {
+//     id: 3,
+//     img: require('../../assets/carouselStuff/image3.jpeg'),
+//     title: 'AirPods Max',
+//     description: 'Effortless AirPods experience',
+//     price: '$549',
+//   },
+// ];
 
 const MultipleChoiceTestScreen = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -118,6 +144,10 @@ const MultipleChoiceTestScreen = () => {
           horizontal
           pagingEnabled
         />
+        {/* <CustomCarousel
+          data={dummyCarousel}
+          renderItem={({item}) => <SlideItem item={item} />}
+        /> */}
       </View>
     </View>
   );
